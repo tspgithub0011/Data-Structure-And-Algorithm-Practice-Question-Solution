87fd46038fc21645eea3f76cfdf6f6573b558c71
Tanmay Patil <157365810+tspgithub0011@users.noreply.github.com>
2025-12-16 14:13:56 +0530

    Update prog5_function_pointer.cpp

--- C++ Programming/C++/Chapter 3 - Pointers and arrays/prog5_function_pointer.cpp
+++ C++ Programming/C++/Chapter 3 - Pointers and arrays/prog5_function_pointer.cpp
@@ -3,3 +3,14 @@
 #include<iostream>
 using namespace std;
 
+int multiply(int a,int b){
+    return a*b;
+}
+
+int main(){
+    int (*func)(int,int);
+    func = multiply;
+    int x = func(2,2);
+    cout<<"Product = "<<x<<endl;
+    return 0;
+}
