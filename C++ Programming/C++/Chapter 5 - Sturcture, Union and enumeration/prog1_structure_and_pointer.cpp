dabf03f43d37ac5f4e5ffe695464d40030ae5545
Tanmay Patil <157365810+tspgithub0011@users.noreply.github.com>
2025-12-19 18:31:16 +0530

    Update prog1_structure_and_pointer.cpp with latest changes

---
+++ C++ Programming/C++/Chapter 5 - Sturcture, Union and enumeration/prog1_structure_and_pointer.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+struct demo
+{
+    int a,b;
+    // this can have constructor, destructor, private and public and private components too..
+    void getData(int x,int y){
+        a=x;
+        b=y;
+    }
+};
+
+int main(){
+    demo d;
+    demo* ptr=&d;
+    ptr->getData(2,3);
+    cout<<"a = "<<ptr->a<<endl;
+    cout<<"b = "<<ptr->b<<endl;
+}
