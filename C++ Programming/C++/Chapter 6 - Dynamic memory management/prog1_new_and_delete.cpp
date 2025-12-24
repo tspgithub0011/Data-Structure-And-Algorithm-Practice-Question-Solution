369b76f19f6e2b7ef0ebdb512fa8588f9923c17c
Tanmay Patil <157365810+tspgithub0011@users.noreply.github.com>
2025-12-24 12:50:52 +0530

    updated code

---
+++ C++ Programming/C++/Chapter 6 - Dynamic memory management/prog1_new_and_delete.cpp
@@ -0,0 +1,10 @@
+// reefer : https://www.geeksforgeeks.org/cpp/new-and-delete-operators-in-cpp-for-dynamic-memory/
+
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main(){
+    int * ptr= new int(6);
+    int* ptrarray = new int[3];
+}
